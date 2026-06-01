4d6e8010c0a7d8dfdfc8499f93eaae63d9f4cf30
utkarsh-0106 <utkarsh.maheshwari0106@gmail.com>
2026-06-01 08:27:15 +0530

    Update full project

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,9 +15,6 @@ app.use(
   cors({
     origin: [
   "http://localhost:5173",
-  // "https://ai-developer-toolbox-czp6.vercel.app",
-  // "https://ai-developer-toolbox-mnwa.vercel.app",
-  "https://ai-dev-database.vercel.app/",
   "https://ai-dev-database-y44d0duxh-utkarsh-0106s-projects.vercel.app",
 ],
   })
